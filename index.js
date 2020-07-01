d638081224021336f36cd8b95d83ca23be3cc795
tinhttgcs18834 <66712957+tinhttgcs18834@users.noreply.github.com>
2020-07-01 16:42:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 var session = require('express-session')
 var path = require('path');
 var router = express.Router();  
